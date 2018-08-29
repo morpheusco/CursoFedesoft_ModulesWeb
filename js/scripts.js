cfca9ffbf638d7ebbe7c603ad93be683c38937e5
Edison Monsalve Cuartas <edison.monsalve@gmail.com>
2018-08-29 16:18:46 -0500

    Incluye animaciones y funcionalidad con JS y Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 // ocultar la barra de navegación primaria
 $(document).ready(function () {
 
-
-   // $("#botonNavOculta").on("click", function () {
-  //      $('#navOculta').toggle();
- //   });
-
-    $('botonNavOculta.toggle').on('click',function() {
+    $('botonNavOculta.toggle').on('click', function () {
         if ($('navOculta.hideMe ').is('[hidden]')) {
             $('navOculta.hideMe ').removeAttr('hidden');
         } else {
-            $('navOculta.hideMe ').attr('hidden','');
+            $('navOculta.hideMe ').attr('hidden', '');
         }
     });
 
+    window.onscroll = function () {
+        flotarImagenes()
+    };
+
+    function flotarImagenes() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            document.getElementById("imgLeft").className = "float-left";
+            document.getElementById("imgRight").className = "float-right";                        
+        }
+        if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
+            document.getElementById("imgLeft").className = "float-right";
+            document.getElementById("imgRight").className = "float-left";
+        }
+        if (document.body.scrollTop > 3500 || document.documentElement.scrollTop > 3500) {
+            document.getElementById("imgLeft").className = "float-left";
+            document.getElementById("imgRight").className = "float-right";            
+        }
+    };
 
 });
-
-
