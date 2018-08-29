1934b1331da55a70c843892d06fe4fcca1820df8
Edison Monsalve Cuartas <edison.monsalve@gmail.com>
2018-08-28 22:10:42 -0500

    Botones interactividad listos, falta la dichosa caja que ingresa.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-// Empty JS for your own code to be here
+// ocultar la barra de navegación primaria
+$(document).ready(function () {
+
+
+   // $("#botonNavOculta").on("click", function () {
+  //      $('#navOculta').toggle();
+ //   });
+
+    $('botonNavOculta.toggle').on('click',function() {
+        if ($('navOculta.hideMe ').is('[hidden]')) {
+            $('navOculta.hideMe ').removeAttr('hidden');
+        } else {
+            $('navOculta.hideMe ').attr('hidden','');
+        }
+    });
+
+
+});
+
+
